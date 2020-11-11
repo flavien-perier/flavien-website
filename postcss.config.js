261f57d6864fa4585006e18a9a6462443a6a5f06
Flavien PERIER <flavien.perier@pm.me>
2020-11-11 22:24:48 +0100

    Change whitelist to safelist

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ const competences = require("./src/data/competences.json").competences;
 const competencesLevels = competences.map(competence => competence.lvl).filter((value, index, self) => self.indexOf(value) === index);
 const competencesTypes = competences.map(competence => competence.type).filter((value, index, self) => self.indexOf(value) === index);
 
-const whitelist = competencesLevels.map(level => "progress-bar-animation-" + level)
+const safelist = competencesLevels.map(level => "progress-bar-animation-" + level)
 						.concat(competencesTypes.map(type => "bg-competence-" + type));
 
 const configuration = {
@@ -13,18 +13,15 @@ const configuration = {
 			.replace(/<script.+?<\/script>/gis, "")
 			.match(/[a-z0-9-_/:]+/gi) || [];
 	},
-	whitelist: [
-		...whitelist,
-		"h1", "h2", "h3", "h4", "h5", "p", "a", "img", "strong",
-		"code", "pre"
-	],
-	whitelistPatterns: [
-		/-(leave|enter|appear)(|-(to|from|active))$/, 
-		/^(?!(|.*?:)cursor-move).+-move$/, 
+	safelist: [
+		...safelist,
+		"h1", "h2", "h3", "h4", "h5", "p", "a", "img", "strong", "code", "pre",
+		/-(leave|enter|appear)(|-(to|from|active))$/,
+		/^(?!(|.*?:)cursor-move).+-move$/,
 		/^router-link(|-exact)-active$/,
-		/data-v-.*/, 
-		/svg.*/, 
-		/fa.*/,
+		/data-v-.*/,
+		/svg.*/,
+		/fa.*/
 	]
 };
 
