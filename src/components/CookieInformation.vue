01188bffe801a806d07c11e0e40bbe0d2ceb5ba8
Flavien PERIER <flavien.perier@pm.me>
2021-01-10 18:18:25 +0100

    Update dependencies

--- src/components/CookieInformation.vue
+++ src/components/CookieInformation.vue
@@ -17,11 +17,11 @@ export default class CookieInformation extends Vue {
 <i18n>
 {
   "en": {
-    "cookieMessage": "This website uses cookies in order to measure its audience. These cookies are and will never be shared or resold to third parties.<br/>If you do not wish to be tracked, I invite you to activate the option \"do not track\" in your browser.",
+    "cookieMessage": "This website uses cookies in order to measure its audience. These cookies are and will never be shared or resold to third parties. If you do not wish to be tracked, I invite you to activate the option \"do not track\" in your browser.",
     "ok": "OK"
   },
   "fr": {
-    "cookieMessage": "Ce site utilise des cookies dans le but de mesurer son audience. Ces derniers ne sont et ne seront jamais, ni partagés, ni revendus à de tierces entités.<br/>Si vous ne souhaitez pas être suivi, je vous invite à activer l'option \"ne pas suivre\" dans votre navigateur.",
+    "cookieMessage": "Ce site utilise des cookies dans le but de mesurer son audience. Ces derniers ne sont et ne seront jamais, ni partagés, ni revendus à de tierces entités. Si vous ne souhaitez pas être suivi, je vous invite à activer l'option \"ne pas suivre\" dans votre navigateur.",
     "ok": "ok"
   }
 }
