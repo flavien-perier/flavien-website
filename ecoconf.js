3c2f6a0f41e475d68d2822f94d953ad64e58d3a3
Flavien PERIER <flavien.perier@pm.me>
2021-09-06 17:30:54 +0200

    Correct `Content-Security-Policy`

--- ecoconf.js
+++ ecoconf.js
@@ -9,7 +9,7 @@ module.exports = {
     "Cache-Control": "max-age=604800",
     "X-XSS-Protection": "1;mode=block",
     "X-Frame-Options": "DENY",
-    "Content-Security-Policy": "default-src 'self' *.flavien.io",
+    "Content-Security-Policy": "default-src 'self' *.flavien.io data:",
   },
   contentType: {},
   proxy: {},
