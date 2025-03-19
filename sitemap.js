9dce5a490ca3ab619a1a34a1a41728f3da9e2704
Flavien PERIER <flavien.perier@pm.me>
2025-03-19 08:43:30 +0100

    Fix sitemap date

--- sitemap.js
+++ sitemap.js
@@ -11,44 +11,54 @@ function getLastmod(axiosResponse) {
     return axiosResponse.data[0].commit.author.date.split("T")[0];
 }
 
-function makeUrl(path, lastmod) {
+function toSitemapDate(dateString) {
+    const date = new Date(dateString);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+}
+
+function makeUrl(path, lastmod, priority) {
     return `    <url>
         <loc>${BASE_URL}${path}</loc>
-        <lastmod>${lastmod}</lastmod>
+        <lastmod>${toSitemapDate(lastmod)}</lastmod>
         <changefreq>monthly</changefreq>
-        <priority>1</priority>
+        <priority>${priority}</priority>
     </url>
 `;
 }
 
 async function makeHome() {
     const response = await axios.get(`${GITHUB_REPO_URL}/flavien-perier/wiki-articles/commits?path=articles/home.md`);
-    return makeUrl(`/`, getLastmod(response));
+    return makeUrl(`/`, getLastmod(response), 1);
 }
 
 async function makeCompetences() {
     const response = await axios.get(`${GITHUB_REPO_URL}/flavien-perier/flavien-website/commits?path=public/data/competences.json`);
-    return makeUrl(`/competences`, getLastmod(response));
+    return makeUrl(`/competences`, getLastmod(response), 0.5);
 }
 
 async function makeExperiences() {
     const response = await axios.get(`${GITHUB_REPO_URL}/flavien-perier/flavien-website/commits?path=public/data/experiences.json`);
-    return makeUrl(`/experiences`, getLastmod(response));
+    return makeUrl(`/experiences`, getLastmod(response), 0.5);
 }
 
 async function makeProjects() {
     const response = await axios.get(`${GITHUB_REPO_URL}/flavien-perier/flavien-website/commits?path=public/data/projects.json`);
-    return makeUrl(`/projects`, getLastmod(response));
+    return makeUrl(`/projects`, getLastmod(response), 0.5);
 }
 
 function makeWikis() {
     const now = new Date();
-    return makeUrl(`/wiki`, `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`);
+    return makeUrl(`/wiki`, now, 0.7);
 }
 
 async function makeWikiUrl(article) {
     const response = await axios.get(`${GITHUB_REPO_URL}/flavien-perier/wiki-articles/commits?path=articles/${article}`);
-    return makeUrl(`/wiki/${article}`, getLastmod(response));
+    return makeUrl(`/wiki/${article}`, getLastmod(response), 1);
 }
 
 async function main() {
